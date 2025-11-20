619a90df4e0beef2787c4c9c8d0f2bd285236209
Azhaan Ali Siddiqui <azhaanalisiddiqui15@gmail.com>
2025-11-21 00:34:09 +0530

    app updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
-function App() {
+import Navbar from "./components/Navbar";
+import ParticlesBackground from "./components/ParticlesBackground";
+import About from "./sections/About";
+import Home from "./sections/Home";
+import Skills from "./sections/Skills";
+import Projects from "./sections/Projects";
+import Experience from "./sections/Experience";
+import Testimonials from "./sections/Testimonials";
+import Contact from "./sections/Contact";
+import Footer from "./sections/Footer";
 
-  return (
-    <>
-    </>
+
+
+export default function App(){
+  return(
+    <div className="relative gradient text-white">
+      <ParticlesBackground/>
+
+
+      <Navbar/>
+      <Home/>
+      <About/>
+      <Skills/>
+      <Projects/>
+      <Experience/>
+      <Testimonials/>
+      <Contact/>
+      <Footer/>
+    </div>
   )
 }
-
-export default App
